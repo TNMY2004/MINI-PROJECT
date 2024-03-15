299e1844022665adc25740ba525aef77997066b6
Tanmay Navhade <navhadetanmay123@gmail.com>
2024-03-15 10:47:10 +0530

    fit

--- TNMY/index.php
+++ TNMY/index.php
@@ -49,8 +49,8 @@
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <!-- <label class="custom-control-label" for="customCheck">Remember
-                          Me</label> -->
+                                                <label class="custom-control-label" for="customCheck">Remember
+                          Me</label> 
                                             </div>
                                         </div>
                                         <div class="form-group">
